3e1f2ff82a75daefa4a104dd895fc7b08c470a03
Matthew A Rosales <matthewrosales@usf.edu>
2024-07-12 19:53:09 -0400

    Create Tasks.js

---
+++ Tasks.js
@@ -0,0 +1,9 @@
+// U22978120
+
+const tasks = [
+  { id: 1, name: "Complete the React challenge", isCompleted: false },
+  { id: 2, name: "complete excel project", isCompleted: false },
+  // I could have added more tasks here
+];
+
+export default tasks;
